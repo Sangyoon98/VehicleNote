6ca7a315b0ad20b7de615fa6263399e52f7c3bd0
Sangyoon <qlrqod123123@naver.com>
2026-02-04 21:15:44 +0900

    feat: presentation 계층 구현

---
+++ app/src/main/java/com/sangyoon/vehiclenote/di/UseCaseModule.kt
@@ -0,0 +1,40 @@
+package com.sangyoon.vehiclenote.di
+
+import com.sangyoon.vehiclenote.domain.repository.VehicleRepository
+import com.sangyoon.vehiclenote.domain.usecase.AddVehicleUseCase
+import com.sangyoon.vehiclenote.domain.usecase.GetAllVehiclesUseCase
+import com.sangyoon.vehiclenote.domain.usecase.SearchVehicleUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGetAllVehiclesUseCase(
+        repository: VehicleRepository
+    ): GetAllVehiclesUseCase {
+        return GetAllVehiclesUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddVehicleUseCase(
+        repository: VehicleRepository
+    ): AddVehicleUseCase {
+        return AddVehicleUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchVehicleUseCase(
+        repository: VehicleRepository
+    ): SearchVehicleUseCase {
+        return SearchVehicleUseCase(repository)
+    }
+}
